7811ffe2344be0a3394d424130fc146c465e25f9
Prashant-kumar-321 <pk6176838@gmail.com>
2024-10-17 11:41:43 +0530

    Set up tailwindcss developement environment

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+  ],
+  theme: {
+    extend: {
+      colors:{
+        primary: '#1c8a7f',
+        secondary: '#81d8d0',
+      }
+    },
+  },
+  plugins: [],
+}
+
