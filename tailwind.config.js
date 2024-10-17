f3e5facfeddb162e672646e1638aa47cc185dd0e
Prashant-kumar-321 <pk6176838@gmail.com>
2024-10-17 15:25:36 +0530

    Finish Home page of mini project of others frontend mini project

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,11 @@ module.exports = {
       colors:{
         primary: '#1c8a7f',
         secondary: '#81d8d0',
+      }, 
+      screens:{
+        tablet: '640px', 
+
+        laptop: '1024px'
       }
     },
   },
